29c59519efc68616a24e57a3ae3d92295783ceca
Michel Mentz Fernandes <michelmf_rs@hotmail.com>
2016-06-27 17:54:03 -0300

    feat(CandidatoController): adicionar validação de email com token de url único.

--- Selecao/Selecao-Web/src/main/java/br/com/crescer/selecao/controller/CandidatoController.java
+++ Selecao/Selecao-Web/src/main/java/br/com/crescer/selecao/controller/CandidatoController.java
@@ -1,25 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package br.com.crescer.selecao.controller;
 
 import br.com.crescer.selecao.captcha.RecaptchaService;
+import br.com.crescer.selecao.entities.Candidato;
+import br.com.crescer.selecao.service.services.CandidatoService;
+import br.com.crescer.selecao.service.services.EmailService;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * @author Murillo
+ *
+ * @author michel.fernandes
  */
 @Controller
-public class InteressadoController {
+public class CandidatoController {
     
     @Autowired
     RecaptchaService recaptchaService;
     
-    @RequestMapping(value="/")
-    String index() {
-        return "Index";
-    } 
+    @Autowired
+    CandidatoService candidatoService;
+    
+    @Autowired
+    EmailService emailService;
     
     @RequestMapping(value="/cadastro", method = RequestMethod.GET)
     String exemplo() {
@@ -27,14 +39,18 @@ public class InteressadoController {
     } 
     
     @RequestMapping(value="/cadastro", method = RequestMethod.POST)
-    String login2(String username, String password, HttpServletRequest req) {
+    String save(@Valid Candidato candidato, HttpServletRequest req) {
         String response = req.getParameter("g-recaptcha-response");
         String ipAcesso = req.getRemoteAddr();
         boolean captchaValido = recaptchaService.isResponseValid(ipAcesso, response);
         
         if(captchaValido) {
             try {
-                //interessadoService.save(INTERESSADO);
+                if(candidatoService.save(candidato) != null){
+                    emailService.enviarEmail(candidato);
+                }else{
+                    //email ja existe
+                }
             } catch (Exception e){
                 return "redirect:cadastro?erroEmail";
             }
@@ -42,4 +58,10 @@ public class InteressadoController {
         }        
         return "redirect:cadastro?erroCaptcha";
     }
+    
+    @ModelAttribute("candidato")
+    public Candidato candidato() {
+        return new Candidato();
+    }
+    
 }
