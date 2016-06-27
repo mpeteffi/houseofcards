9c58c50c185df53316b786c856d0e6dd2d7f45f5
mpeteffi <murillo.peteffi@hotmail.com>
2016-06-27 17:12:16 -0300

    feat(EmailController): adicionar método de confirmação de token.

--- Selecao/Selecao-Web/src/main/java/br/com/crescer/selecao/controller/EmailController.java
+++ Selecao/Selecao-Web/src/main/java/br/com/crescer/selecao/controller/EmailController.java
@@ -1,6 +1,7 @@
 package br.com.crescer.selecao.controller;
 
 import br.com.crescer.selecao.service.services.EmailService;
+import br.com.crescer.selecao.service.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +18,27 @@ public class EmailController {
     @Autowired
     EmailService emailService;
     
+    @Autowired
+    TokenService tokenService;
+    
     @RequestMapping(value="/enviar", method = RequestMethod.GET)
     String enviarEmail() {
         
         //emailService.enviarEmail(, "token gigante");
         
         return "administrativo";
+    } 
+    
+    @RequestMapping(value="/confirmar", method = RequestMethod.GET)
+    String confirmarToken(String token) {
+        
+        Boolean confirmado = tokenService.confirmarInteresse(token);
+        
+        if(confirmado){
+            return "paginaSucesso";
+        } else {
+            return "paginaErro";
+        }
     } 
     
 }
