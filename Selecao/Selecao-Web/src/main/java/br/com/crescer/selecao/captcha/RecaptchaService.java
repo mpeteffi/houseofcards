35ca2be77138c1e12fa6deb12e0d44b93ad14761
Murillo Peteffi <Murillo Peteffi>
2016-06-27 08:41:49 -0300

    feat(Captcha): adicionar servico de captcha e configuração necessária.

---
+++ Selecao/Selecao-Web/src/main/java/br/com/crescer/selecao/captcha/RecaptchaService.java
@@ -0,0 +1,59 @@
+package br.com.crescer.selecao.captcha;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Murillo - Seguindo modelo de Bartosz Kiełczewski - kielczewski.eu/2015/07/spring-recaptcha-v2-form-validation/
+ */
+@Service
+public class RecaptchaService {
+
+    private static class RecaptchaResponse {
+        @JsonProperty("success")
+        private boolean success;
+        @JsonProperty("error-codes")
+        private Collection<String> errorCodes;
+    }
+
+    private final RestTemplate restTemplate;
+
+    @Value("${recaptcha.url}")
+    private String recaptchaUrl;
+
+    @Value("${recaptcha.secret-key}")
+    private String recaptchaSecretKey;
+
+    @Autowired
+    public RecaptchaService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public boolean isResponseValid(String remoteIp, String response) {
+        RecaptchaResponse recaptchaResponse;
+        try {
+            recaptchaResponse = restTemplate.postForEntity(
+                    recaptchaUrl, createBody(recaptchaSecretKey, remoteIp, response), RecaptchaResponse.class)
+                    .getBody();
+            return recaptchaResponse.success;
+        } catch (RestClientException e) {
+            //
+        }
+        return false;
+    }
+
+    private MultiValueMap<String, String> createBody(String secret, String remoteIp, String response) {
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+        form.add("secret", secret);
+        form.add("remoteip", remoteIp);
+        form.add("response", response);
+        return form;
+    }
+}
---
+++ Selecao/Selecao-Web/src/main/java/br/com/crescer/selecao/captcha/RestTemplateConfig.java
@@ -0,0 +1,34 @@
+package br.com.crescer.selecao.captcha;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Murillo - Seguindo modelo de Bartosz Kiełczewski - kielczewski.eu/2015/07/spring-recaptcha-v2-form-validation/
+ */
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory httpRequestFactory) {
+        RestTemplate restTemplate = new RestTemplate(httpRequestFactory);
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        return restTemplate;
+    }
+
+    @Bean
+    public ClientHttpRequestFactory httpRequestFactory(HttpClient httpClient) {
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClientBuilder.create().build();
+    }
+}
