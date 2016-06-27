a480ab2fec84c019a8ec099769f0b35c74a4f819
Michel Mentz Fernandes <michelmf_rs@hotmail.com>
2016-06-27 12:35:43 -0300

    feat(EmailController): adicionar controller para os emails.

---
+++ Selecao/Selecao-Web/src/main/java/br/com/crescer/selecao/controller/EmailController.java
@@ -0,0 +1,28 @@
+package br.com.crescer.selecao.controller;
+
+import br.com.crescer.selecao.service.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author michel.fernandes
+ */
+@Controller
+@RequestMapping("/email")
+public class EmailController {
+    
+    @Autowired
+    EmailService emailService;
+    
+    @RequestMapping(value="/enviar", method = RequestMethod.GET)
+    String enviarEmail() {
+        
+        emailService.enviarEmail("michelmf_rs@hotmail.com", "Assunto", "Mensagem");
+        
+        return "administrativo";
+    } 
+    
+}
