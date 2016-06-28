1eb41638e801aefd2256a3847a728154d67983d8
Andrews kuhn <andrewskuhn@hotmail.com>
2016-06-28 01:42:18 -0300

    feat(ControllerNovaEdição) - adicionado nova edição.

--- Selecao/Selecao-Web/src/main/java/br/com/crescer/selecao/controller/AdministrativoController.java
+++ Selecao/Selecao-Web/src/main/java/br/com/crescer/selecao/controller/AdministrativoController.java
@@ -1,28 +1,45 @@
 package br.com.crescer.selecao.controller;
 
 import br.com.crescer.selecao.entities.Informacao;
+import br.com.crescer.selecao.entities.Processoseletivo;
 import br.com.crescer.selecao.service.services.CandidatoService;
+import br.com.crescer.selecao.service.services.ProcessoseletivoService;
 import java.util.Calendar;
 import java.util.Date;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author Murillo
  */
 @Controller
-@RequestMapping(value="/administrativo")
 public class AdministrativoController {
     
     @Autowired
     CandidatoService candidatoservice;
     
-    @RequestMapping(value="")
+    @Autowired
+    ProcessoseletivoService processoSeletivoService;
+    
+    @RequestMapping(value="/administrativo")
     String login() {
         return "Administrativo";
     } 
+    
+    @RequestMapping(value="/novaedicao")
+    String novaEdicao() {
+        return "nova-edicao";
+    } 
+    
+    @RequestMapping(value="/cadastro-nova-edicao", method = RequestMethod.POST)
+    String save(@Valid Processoseletivo processoseletivo,Model model) {
+        processoSeletivoService.save(processoseletivo);
+       return "Administrativo";
+    }
 }
