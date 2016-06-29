e817a0c96df39db60bb93b3f17ce5e6918087adc
mpeteffi <murillo.peteffi@hotmail.com>
2016-06-28 21:30:44 -0300

    test(UsuarioService): adicionar classe de testes para UsuarioService.

---
+++ Selecao/Selecao-Service/src/test/java/br/com/crescer/selecao/service/services/UsuarioServiceTest.java
@@ -0,0 +1,44 @@
+package br.com.crescer.selecao.service.services;
+
+import br.com.crescer.selecao.entities.Token;
+import br.com.crescer.selecao.entities.Usuario;
+import br.com.crescer.selecao.service.repository.UsuarioRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * @author Murillo
+ */
+public class UsuarioServiceTest {
+    
+    @InjectMocks
+    private UsuarioService usuarioService;
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        when(usuarioRepository.findOneByEmail("EmailValido")).thenReturn(new Usuario());
+        when(usuarioRepository.findOneByEmail("EmailInvalido")).thenReturn(null);
+    }
+    
+    @Test
+    public void findByEmailValidoRetornaUser() {
+        Usuario user = usuarioService.findOneByEmail("EmailValido");
+        assertTrue(user != null);
+    }
+    
+    @Test
+    public void findByEmailInvalidoRetornaNull() {
+        Usuario user = usuarioService.findOneByEmail("EmailInvalido");
+        assertTrue(user == null);
+    }
+}
