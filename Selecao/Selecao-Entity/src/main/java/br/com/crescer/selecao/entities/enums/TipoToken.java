ddd3f1c1bc54d90a429384c7eadfecc89cc0fa10
Michel Mentz Fernandes <michelmf_rs@hotmail.com>
2016-06-30 15:52:11 -0300

    refactor(Enum): alterar classes de enum que estavam complexas

--- Selecao/Selecao-Entity/src/main/java/br/com/crescer/selecao/entities/enums/TipoToken.java
+++ Selecao/Selecao-Entity/src/main/java/br/com/crescer/selecao/entities/enums/TipoToken.java
@@ -5,18 +5,7 @@ package br.com.crescer.selecao.entities.enums;
  * @author michel.fernandes
  */
 public enum TipoToken {
-    INTERESSADO("INTERESSADO"),
-    INSCRICAO("INSCRICAO"),
-    ENTREVISTA("ENTREVISTA")
-    ;
-    private final String text;
-    
-    private TipoToken(final String text) {
-        this.text = text;
-    }
-
-    @Override
-    public String toString() {
-        return text;
-    }
+    INTERESSADO,
+    INSCRICAO,
+    ENTREVISTA    
 }
