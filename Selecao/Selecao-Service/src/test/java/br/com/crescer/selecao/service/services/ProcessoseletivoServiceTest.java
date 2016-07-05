aa178f0f69a7e070b897e1a1ba54ab0461702caa
mpeteffi <murillo.peteffi@hotmail.com>
2016-07-05 04:35:13 -0300

    test(ProcessoSeletivo): adicionar testes à service.

--- Selecao/Selecao-Service/src/test/java/br/com/crescer/selecao/service/services/ProcessoseletivoServiceTest.java
+++ Selecao/Selecao-Service/src/test/java/br/com/crescer/selecao/service/services/ProcessoseletivoServiceTest.java
@@ -3,7 +3,10 @@ package br.com.crescer.selecao.service.services;
 import br.com.crescer.selecao.entities.Candidato;
 import br.com.crescer.selecao.entities.Processoseletivo;
 import br.com.crescer.selecao.service.repository.ProcessoseletivoRepository;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -44,15 +47,24 @@ public class ProcessoseletivoServiceTest {
         assertTrue(processo != null);
     } 
     
-//    @Test
-//    public void salvarProcessoValidoRetornaTrue() {
-//        boolean save = processoseletivoService.save(new Processoseletivo()) ;
-//        assertTrue(save);
-//    } 
-//    
-//    @Test
-//    public void salvarProcessoInvalidoRetornaFalse() {
-//        boolean save = processoseletivoService.save(null);
-//        assertFalse(save);
-//    } 
+    @Test
+    public void salvarProcessoValidoRetornaTrue() throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        Processoseletivo valido = new Processoseletivo("2016/02", format.parse("01/08/2016"), format.parse("02/08/2016"), format.parse("03/08/2016"), format.parse("04/08/2016"));
+        boolean save = processoseletivoService.criarProcessoSeletivo(valido);
+        assertTrue(save);
+    } 
+    
+    @Test
+    public void salvarProcessoInvalidoRetornaFalse() throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        Processoseletivo valido = new Processoseletivo("2016/02", format.parse("05/08/2016"), format.parse("02/08/2016"), format.parse("03/08/2016"), format.parse("04/08/2016"));
+        boolean save = processoseletivoService.criarProcessoSeletivo(valido);
+        assertFalse(save);
+    } 
+    
+    @Test(expected=NullPointerException.class)
+    public void salvarProcessoNullRetornaNullPointerException(){
+        boolean save = processoseletivoService.criarProcessoSeletivo(null);
+    } 
 }
